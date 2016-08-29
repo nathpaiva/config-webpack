182c369ebe738351174cdb4c25be7b763fe48c4c
Nathalia Paiva <hello@nathpaiva.com.br>
2016-08-29 18:53:14 -0300

    config webpack

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var React = require('react');
 
 var Title = React.createClass({
     render: function () {
-        return React.createElement('h1', null, 'Hello World');
+        return React.createElement('h1', null, 'Teste');
     }
 });
 
