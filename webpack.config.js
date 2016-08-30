7a74a2596426643adff3cf32b3237a4425ce2191
Nathalia Paiva <hello@nathpaiva.com.br>
2016-08-29 21:07:59 -0300

    config babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  }
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        laoders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader: 'babel'
+        }]
+    }
 }
