4d5be721d60c096d546a697dd9c8c4efe95d33f0
Nathalia Paiva <hello@nathpaiva.com.br>
2016-08-30 21:56:40 -0300

    config hot laoder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 'use strict';
 
-import Title from './app';
-
 import React from 'react';
 // short notation
-// react-dom
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './app';
 
 
 render(
-    <Title />,
+    <AppContainer >
+        <App />
+    </AppContainer>,
     document.querySelector('[data-js="app"]')
-)
+);
+
+if (module.hot) {
+    module.hot.accept('./app', () => {
+        const NextApp = require('./app').default;
+
+        render(
+            <AppContainer >
+                <NextApp />
+            </AppContainer>,
+            document.querySelector('[data-js="app"]')
+        );
+    });
+}
